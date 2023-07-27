4c3936b9a45aac95af846a917a9662f6a64e300d
jp <jonahpalmer@gmail.com>
2023-07-27 18:03:16 -0400

    finish autocorrect (manual lists)

--- vin/vinapp/forms.py
+++ vin/vinapp/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import UserAnswers
+import json
 
 class UserAnswersForm(forms.ModelForm):
 
+    # replace these with dynamically generated lists
     GRAPE_CHOICES = ("Chardonnay", "Chenin Blanc", "Gewurtraminer",
     "Pinot Gris/Grigio", "Riesling", "Sauvignon Blanc", "Cabernet Sauvignon",
     "Merlot,", "Pinot Noir", "Syrah/Shiraz", "Grenache-based blend",
@@ -10,9 +12,35 @@ class UserAnswersForm(forms.ModelForm):
     "Corvina-based blend", "Sangiovese", "Tempranillo", "Malbec",
     "Gamay", "Cabernet Franc")
 
+    COUNTRY_CHOICES = ("France", "Italy", "USA", "Germany",
+    "Spain", "Argentina", "Australia", "New Zealand")
+
+    REGION_CHOICES = ("Burgundy", "California", "Loire", "Alsace", "Mosel",
+    "South Island", "Bordeaux", "Northern Rhone", "Southern Rhone", "Piedmont",
+    "Veneto", "Tuscany", "La Rioja", "Cuyo", "South Australia")
+
+    APPELLATION_CHOICES = ("Chablis 1er Cru", "Chassagne-Montratcher 1er Cru",
+    "Napa Valley", "Vouvray", "Alsace Grand Cru", "Pinot Grigio Delle Venezie",
+    "Mosel", "Sancerre", "Marlborough", "St-Julien", "Saint-Emilion",
+    "Borgogne 1er Cru", "Cote-Rotie", "Chateauneuf-Du-Pape", "Barolo",
+    "Amarone Della Valpolicella", "Brunello Di Montalcino Riserva",
+    "Rioja Gran Reserva", "Russian River Valley", "Mendoza", "Barossa Valley",
+    "Central Otago", "Borgogne")
+
     class Meta:
         model = UserAnswers
         fields = ['grape', 'country', 'region', 'appellation', 'vintage']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['grape'].widget.attrs.update({'class': 'autocomplete',
+        'data-choices': json.dumps(self.GRAPE_CHOICES)})
+        self.fields['country'].widget.attrs.update({'class': 'autocomplete',
+        'data-choices': json.dumps(self.COUNTRY_CHOICES)})
+        self.fields['region'].widget.attrs.update({'class': 'autocomplete',
+        'data-choices': json.dumps(self.REGION_CHOICES)})
+        self.fields['appellation'].widget.attrs.update({'class': 'autocomplete',
+        'data-choices': json.dumps(self.APPELLATION_CHOICES)})
 
 class MainPageForm(forms.Form):
     SCOPE_CHOICES = [
