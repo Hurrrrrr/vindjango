520b1639dc462029d2480cd3f386302caadeecc2
jp <jonahpalmer@gmail.com>
2023-09-09 15:53:52 -0400

    add favicon

--- vin/vin/settings.py
+++ vin/vin/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vinapp'
+    'vinapp',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
